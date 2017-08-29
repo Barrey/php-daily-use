d62b23b6961a6b3bd33ad2d436bd796fef8e593d
ajibari <ajibari@gmail.com>
2017-08-29 22:39:16 +0700

    add fizzbuzz

---
+++ arithmetic/fizzbuzz.php
@@ -0,0 +1,21 @@
+<?php
+function fizzbuzz($max){
+	for ($i = 1; $i <= $max; $i++)
+	{
+		if($i % 3 == 0 && $i % 5 ==0){
+			echo "FizzBuzz<br />";
+		}
+		else if($i % 3 == 0){
+			echo "Fizz<br />";
+		}
+		else if($i % 5 == 0){
+			echo "Buzz<br />";
+		}
+		else {
+			echo $i."<br />";
+		}
+	}
+}
+
+fizzbuzz(50);
+?>
